82e987c3fbe2022380122650d32522917f06dba3
Lucas <lucascampardo@outlook.com>
2022-12-05 14:21:39 -0300

    todos os campeões da copa do mundo

---
+++ WorldCup.py
@@ -0,0 +1,9 @@
+def world_cup():
+    pentacampeao = 'Brasil' 
+    tetracampeao = 'Alemanha e Italia'
+    bicampeao = 'Argentina, Uruguai e França'
+    campeao = 'Espanha e Inglaterra'
+
+    print(f"Só há uma nação pentacampeã mundial que é o {pentacampeao}, depois há 2 nações tetracampeãs sendo elas a {tetracampeao}. Não há nenhum tricampeão, mas tem 3 bicampeões sendo eles {bicampeao} e 2 nações que ganharam a Copa do Mundo apenas uma vez, sendo elas {campeao}")
+
+world_cup()
